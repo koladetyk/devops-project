9cf353b0556aced5d6c112be1ab9731543badc97
Ayokolade Erinle <koladeerinle@ayokolades-mbp.home>
2024-06-16 13:47:52 -0400

    new tests

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,13 @@
 const sum = require('./sum');
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 + 3 to equal 6', () => {
+    expect(sum(3, 3)).toBe(6);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
 });
